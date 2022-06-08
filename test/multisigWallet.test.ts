9a5912f185ddbb647bbd62275100bb9588b408c7
PABlond <pierre-alexis.blond@live.fr>
2022-06-08 21:32:03 +0200

    create multisig tests

--- test/multisigWallet.test.ts
+++ test/multisigWallet.test.ts
@@ -3,180 +3,230 @@ import { Contract } from "ethers";
 import { ethers } from "hardhat";
 
 describe("MultisigWallet", function () {
-  // it("should not withdraw if all owners not confirmed the transaction", async function () {
-  //   const Multisig = await ethers.getContractFactory("MultisigWallet");
-  //   const multisig = await Multisig.deploy();
-  //   await multisig.deployed();
-  //   const provider = ethers.provider;
-
-  //   const [owner, bob] = await ethers.getSigners();
-  //   const initBalance = parseFloat(
-  //     ethers.utils.formatEther(await provider.getBalance(bob.address))
-  //   );
-  //   await multisig.deposit({ value: ethers.utils.parseEther("1") });
-  //   await multisig.addOwner(bob.address);
-  //   await multisig.initWithdraw(ethers.utils.parseEther("0.5"), bob.address);
-
-  //   const finalBalance = parseFloat(
-  //     ethers.utils.formatEther(await provider.getBalance(bob.address))
-  //   );
-  //   expect(finalBalance - initBalance).to.be.equal(0);
-  // });
-
-  // it("should not withdraw because an owner can't confirm twice", async function () {
-  //   const Multisig = await ethers.getContractFactory("MultisigWallet");
-  //   const multisig = await Multisig.deploy();
-  //   await multisig.deployed();
-  //   const provider = ethers.provider;
-
-  //   const [owner, bob] = await ethers.getSigners();
-  //   const initBalance = parseFloat(
-  //     ethers.utils.formatEther(await provider.getBalance(bob.address))
-  //   );
-  //   await multisig.deposit({ value: ethers.utils.parseEther("1") });
-  //   await multisig.addOwner(bob.address);
-  //   await multisig.initWithdraw(ethers.utils.parseEther("0.5"), bob.address);
-  //   try {
-  //     await multisig.confirm();
-  //   } catch (e) {
-  //     console.log(e);
-  //   }
-
-  //   const finalBalance = parseFloat(
-  //     ethers.utils.formatEther(await provider.getBalance(bob.address))
-  //   );
-  //   expect(finalBalance - initBalance).to.be.equal(0);
-  // });
-
-  // it("should withdraw when all owners confirm the transaction", async function () {
-  //   const Multisig = await ethers.getContractFactory("MultisigWallet");
-  //   const multisig = await Multisig.deploy();
-  //   await multisig.deployed();
-  //   const provider = ethers.provider;
-
-  //   const [owner, bob] = await ethers.getSigners();
-  //   const initBalance = parseFloat(
-  //     ethers.utils.formatEther(await provider.getBalance(bob.address))
-  //   );
-  //   await multisig.deposit({ value: ethers.utils.parseEther("1") });
-  //   await multisig.addOwner(bob.address);
-  //   await multisig.initWithdraw(ethers.utils.parseEther("0.5"), bob.address);
-  //   await multisig.connect(bob).confirm();
-  //   console.log(await multisig.pendingTransaction())
-
-  //   const finalBalance = parseFloat(
-  //     ethers.utils.formatEther(await provider.getBalance(bob.address))
-  //   );
-  //   console.log(await multisig.pendingTransaction())
-  //   expect(finalBalance - initBalance).to.be.greaterThan(0);
-  // });
-
-  // it("should get pending transaction", async function () {
-  //   const Multisig = await ethers.getContractFactory("MultisigWallet");
-  //   const multisig = await Multisig.deploy();
-  //   await multisig.deployed();
-  //   const provider = ethers.provider;
-
-  //   const [owner, bob] = await ethers.getSigners();
-
-  //   await multisig.initWithdraw(ethers.utils.parseEther("0.5"), bob.address);
-
-  //   const response = await multisig.pendingTransaction()
-  // });
-
-  it("Request new owner", async () => {
-    const Multisig = await ethers.getContractFactory("Owners");
-    const multisig = await Multisig.deploy();
-    await multisig.deployed();
-    const provider = ethers.provider;
-    const [owner, bob, alice] = await ethers.getSigners();
-    console.log(multisig.address);
-    const events = (
-      await (await multisig.initNewOwner(bob.address, [owner.address])).wait()
-    ).events as any[];
-
-    expect(events[0].event).to.equal("AllowOwnerStep");
-    expect(events.length).to.be.greaterThan(0);
-    expect(events[0].args.length).to.be.greaterThan(0);
-    expect(events[0].args[0]).to.equal(owner.address);
-    expect(events[0].args[3]).to.equal(bob.address);
-    expect(events[0].args[1]).to.equal(0);
-  });
-
-  it("Request new owner but failed because new owner already a owner", async () => {
+  it("should not init withdraw because last init is too close", async () => {
     const Multisig = await ethers.getContractFactory("MultisigWallet");
     const multisig = await Multisig.deploy();
     await multisig.deployed();
-    const provider = ethers.provider;
-    multisig.on("NewPendingOwner", (props) => console.log(props));
-    const [owner, bob, alice] = await ethers.getSigners();
-
-    try {
-      expect(
-        await multisig.initNewOwner(owner.address, [owner.address])
-      ).to.throw("");
-    } catch (e) {
-      expect((e as Error).message).to.equal(
-        "VM Exception while processing transaction: reverted with reason string 'Address already an owner'"
-      );
-    }
-  });
-
-  it("Request new owner but failed because needed owner is not an owner", async () => {
-    const Multisig = await ethers.getContractFactory("MultisigWallet");
-    const multisig = await Multisig.deploy();
-    await multisig.deployed();
-    const provider = ethers.provider;
-    multisig.on("NewPendingOwner", (props) => console.log(props));
-    const [owner, bob, alice] = await ethers.getSigners();
-
-    try {
-      expect(
-        await multisig.initNewOwner(bob.address, [alice.address])
-      ).to.throw("");
-    } catch (e) {
-      expect((e as Error).message).to.equal(
-        "VM Exception while processing transaction: reverted with reason string 'Need a list of owner'"
-      );
-    }
-  });
-
-  it("Confirm new owner", async () => {
-    const Multisig = await ethers.getContractFactory("MultisigWallet");
-    const multisig = await Multisig.deploy();
-    await multisig.deployed();
-    const provider = ethers.provider;
-
-    const [owner, bob, alice] = await ethers.getSigners();
-    await multisig.initNewOwner(bob.address, [owner.address]);
-    const events = (await (await multisig.confirmNewOwner(bob.address)).wait())
-      .events as any[];
-
-    expect(events[0].event).to.equal("AllowOwnerStep");
-    expect(events.length).to.be.greaterThan(0);
-    expect(events[0].args.length).to.be.greaterThan(0);
-    expect(events[0].args[4].length).to.equal(1);
-    expect(events[0].args[4][0]).to.equal(owner.address);
-    expect(events[0].args[4].length).to.equal(events[0].args[5].length);
-    expect(events[0].args[1]).to.equal(1);
-  });
-
-  it("Exec new owner", async () => {
-    const Multisig = await ethers.getContractFactory("MultisigWallet");
-    const multisig = await Multisig.deploy();
-    await multisig.deployed();
-    const provider = ethers.provider;
-
-    const [owner, bob, alice] = await ethers.getSigners();
+    const [owner, bob] = await ethers.getSigners();
+    const TestToken = await ethers.getContractFactory("FakeERC20");
+    const testToken = await TestToken.deploy();
+    await testToken.deployed();
+    await testToken.transfer(multisig.address, ethers.utils.parseEther("10"));
     await multisig.initNewOwner(bob.address, [owner.address]);
     await multisig.confirmNewOwner(bob.address);
-    const events = (await (await multisig.execNewOwner(bob.address)).wait())
-      .events as any[];
+    await multisig.execNewOwner(bob.address);
+    await multisig.initWithdraw(
+      ethers.utils.parseEther("1"),
+      bob.address,
+      [owner.address, bob.address],
+      testToken.address
+    );
+    try {
+      await multisig.initWithdraw(
+        ethers.utils.parseEther("1"),
+        bob.address,
+        [owner.address, bob.address],
+        testToken.address
+      );
+    } catch (e) {
+      expect(
+        (e as Error).message.includes(
+          "Need at least 10minutes before each withdraw initialization"
+        )
+      ).to.equal(true);
+    }
+  });
 
-    expect(events[0].event).to.equal("AllowOwnerStep");
-    expect(events.length).to.be.greaterThan(0);
-    expect(events[0].args.length).to.be.greaterThan(0);
-    expect(events[0].args[4].length).to.equal(events[0].args[5].length);
-    expect(events[0].args[1]).to.equal(2);
+  it("should not init withdraw because it needs at least two signers to confirm", async () => {
+    const Multisig = await ethers.getContractFactory("MultisigWallet");
+    const multisig = await Multisig.deploy();
+    await multisig.deployed();
+    const [owner, bob] = await ethers.getSigners();
+    const TestToken = await ethers.getContractFactory("FakeERC20");
+    const testToken = await TestToken.deploy();
+    await testToken.deployed();
+    await testToken.transfer(multisig.address, ethers.utils.parseEther("10"));
+    await multisig.initNewOwner(bob.address, [owner.address]);
+    await multisig.confirmNewOwner(bob.address);
+    await multisig.execNewOwner(bob.address);
+    try {
+      await multisig.initWithdraw(
+        ethers.utils.parseEther("1"),
+        bob.address,
+        [owner.address],
+        testToken.address
+      );
+    } catch (e) {
+      expect(
+        (e as Error).message.includes(
+          "Need at least 2 address to confirm the transaction"
+        )
+      ).to.equal(true);
+    }
+  });
+
+  it("should init withdraw", async () => {
+    const Multisig = await ethers.getContractFactory("MultisigWallet");
+    const multisig = await Multisig.deploy();
+    await multisig.deployed();
+    const [owner, bob] = await ethers.getSigners();
+    const TestToken = await ethers.getContractFactory("FakeERC20");
+    const testToken = await TestToken.deploy();
+    await testToken.deployed();
+    await testToken.transfer(multisig.address, ethers.utils.parseEther("10"));
+    await multisig.initNewOwner(bob.address, [owner.address]);
+    await multisig.confirmNewOwner(bob.address);
+    await multisig.execNewOwner(bob.address);
+
+    const {
+      events: [
+        {
+          args: [step, _, to, confirmedBy, neededOwners, amount, token],
+          event,
+        },
+      ],
+    } = (await (
+      await multisig.initWithdraw(
+        ethers.utils.parseEther("1"),
+        bob.address,
+        [owner.address, bob.address],
+        testToken.address
+      )
+    ).wait()) as any;
+
+    expect(event).to.equal("WithdrawStep");
+    expect(step).to.equal(0);
+    expect(to).to.equal(bob.address);
+    expect(confirmedBy.length).to.equal(0);
+    expect(neededOwners.length).to.equal(2);
+    expect(neededOwners.includes(owner.address)).to.equal(true);
+    expect(neededOwners.includes(bob.address)).to.equal(true);
+    expect(token).to.equal(testToken.address);
+  });
+
+  it("should confirm withdraw", async () => {
+    const Multisig = await ethers.getContractFactory("MultisigWallet");
+    const multisig = await Multisig.deploy();
+    await multisig.deployed();
+    const [owner, bob] = await ethers.getSigners();
+    const TestToken = await ethers.getContractFactory("FakeERC20");
+    const testToken = await TestToken.deploy();
+    await testToken.deployed();
+    await testToken.transfer(multisig.address, ethers.utils.parseEther("10"));
+    await multisig.initNewOwner(bob.address, [owner.address]);
+    await multisig.confirmNewOwner(bob.address);
+    await multisig.execNewOwner(bob.address);
+    await multisig.initWithdraw(
+      ethers.utils.parseEther("1"),
+      bob.address,
+      [owner.address, bob.address],
+      testToken.address
+    );
+    const {
+      events: [
+        {
+          args: [step, _, to, confirmedBy, neededOwners, amount, token],
+          event,
+        },
+      ],
+    } = (await (await multisig.confirmWithdraw()).wait()) as any;
+
+    expect(event).to.equal("WithdrawStep");
+    expect(step).to.equal(1);
+    expect(to).to.equal(bob.address);
+    expect(confirmedBy.length).to.equal(1);
+    expect(neededOwners.length).to.equal(2);
+    expect(neededOwners.includes(owner.address)).to.equal(true);
+    expect(neededOwners.includes(bob.address)).to.equal(true);
+    expect(token).to.equal(testToken.address);
+  });
+
+  it("should fail if not all signers confirmed the transaction", async () => {
+    const Multisig = await ethers.getContractFactory("MultisigWallet");
+    const multisig = await Multisig.deploy();
+    await multisig.deployed();
+    const [owner, bob] = await ethers.getSigners();
+    const TestToken = await ethers.getContractFactory("FakeERC20");
+    const testToken = await TestToken.deploy();
+    await testToken.deployed();
+    await testToken.transfer(multisig.address, ethers.utils.parseEther("10"));
+    const initBalance = parseFloat(
+      ethers.utils.formatEther(await testToken.balanceOf(bob.address))
+    );
+    expect(initBalance).to.equal(0);
+
+    await multisig.initNewOwner(bob.address, [owner.address]);
+    await multisig.confirmNewOwner(bob.address);
+    await multisig.execNewOwner(bob.address);
+    await multisig.initWithdraw(
+      ethers.utils.parseEther("1"),
+      bob.address,
+      [owner.address, bob.address],
+      testToken.address
+    );
+    await multisig.connect(bob).confirmWithdraw();
+
+    try {
+      await multisig.execWithdraw();
+    } catch (e) {
+      expect((e as Error).message.includes("Confirmation step not over")).to.equal(true);
+    }
+
+    const endBalance = parseFloat(
+      ethers.utils.formatEther(await testToken.balanceOf(bob.address))
+    );
+
+    expect(endBalance).to.be.equal(0);
+  });
+
+  it("should withdraw", async () => {
+    const Multisig = await ethers.getContractFactory("MultisigWallet");
+    const multisig = await Multisig.deploy();
+    await multisig.deployed();
+    const [owner, bob] = await ethers.getSigners();
+    const TestToken = await ethers.getContractFactory("FakeERC20");
+    const testToken = await TestToken.deploy();
+    await testToken.deployed();
+    await testToken.transfer(multisig.address, ethers.utils.parseEther("10"));
+    const initBalance = parseFloat(
+      ethers.utils.formatEther(await testToken.balanceOf(bob.address))
+    );
+    expect(initBalance).to.equal(0);
+
+    await multisig.initNewOwner(bob.address, [owner.address]);
+    await multisig.confirmNewOwner(bob.address);
+    await multisig.execNewOwner(bob.address);
+    await multisig.initWithdraw(
+      ethers.utils.parseEther("1"),
+      bob.address,
+      [owner.address, bob.address],
+      testToken.address
+    );
+    await multisig.confirmWithdraw();
+    await multisig.connect(bob).confirmWithdraw();
+    const {
+      events: [
+        _,
+        {
+          args: [step, date, to, confirmedBy, neededOwners, amount, token],
+          event,
+        },
+      ],
+    } = (await (await multisig.execWithdraw()).wait()) as any;
+
+    expect(event).to.equal("WithdrawStep");
+    expect(step).to.equal(2);
+    expect(to).to.equal(bob.address);
+    expect(confirmedBy.length).to.equal(2);
+    expect(neededOwners.length).to.equal(2);
+    expect(neededOwners.includes(owner.address)).to.equal(true);
+    expect(neededOwners.includes(bob.address)).to.equal(true);
+    expect(token).to.equal(testToken.address);
+
+    const endBalance = parseFloat(
+      ethers.utils.formatEther(await testToken.balanceOf(bob.address))
+    );
+
+    expect(endBalance).to.be.equal(1);
   });
 });
